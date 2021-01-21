07d454d73299a796963a3ef04e100af296ea2c25
smartdevello <77758615+smartdevello@users.noreply.github.com>
2021-01-20 22:37:43 -0500

    final py

--- main.py
+++ main.py
@@ -57,151 +57,144 @@ def get_all_binance(symbol, kline_size, save = False):
     print('All caught up..!')
     return data_df
 
-def f(sc):
-    binance_symbols = ["ETHUSDT"]
-    for symbol in binance_symbols:
-        get_all_binance(symbol, '5m', save = True)
-    df = pd.read_csv(r"NANOUSDT-5m-data.csv", parse_dates=True)
-    df = df.tail(8640)
-    #Computing indicators SMA and ADX, and whether trend is bear or bull according to sma
-    df['sma_fast'] = ta.SMA(df['close'],95)
-    df['sma_slow'] = ta.SMA(df['close'],200)
-    df['upper_band'], df['middle_band'], df['lower_band'] = ta.BBANDS(df['close'], timeperiod =20)
-    df['rsi'] = ta.RSI(df['close'],15)
-    df['adx'] = ta.ADX(df.high, df.low, df.close, timeperiod=3)
-    df['bear0bull1'] = np.where((df["sma_fast"] < df["sma_slow"]), 0, 1)
-    # Computing if trend is real according to number of consecutive rows with equal values in bear0bull1 column        
-    df['uptrend'] = 0
-    from itertools import islice
-    for index, row in islice(df.iterrows(), 1, None):
-        if ((row["bear0bull1"]) == 1):
-            df.loc[index, 'uptrend'] = df.loc[index - 1, 'uptrend'] +1
-        else:
-            df.loc[index, 'uptrend'] = 0
-    # Computing RSI so that it is different according to the value in uptrend column
-    for index, row in df.iterrows():
-        if ((row["uptrend"]) > 9):
-            df['rsi'] = ta.RSI(df['close'],10)
-    # Computing general tresholds for RSI according to uptrend column 
-    df['rsi_high'] = np.where((df["uptrend"] < 10), 50, 75)
-    df['rsi_low'] = np.where((df["uptrend"] < 10), 30, 60)
-    # Adjusting RSI treshold according to extreme values of ADX 
-    df.loc[(df['adx'] > 70) & (df['uptrend'] < 10), 'rsi_high'] = df['rsi_high'] + 10 #+20
-    df.loc[(df['adx'] < 50) & (df['uptrend'] < 10), 'rsi_low'] = df['rsi_low'] -8
-    df.loc[(df['adx'] > 70) & (df['uptrend'] > 9), 'rsi_high'] = df['rsi_high'] + 7
-    df.loc[(df['adx'] < 50) & (df['uptrend'] > 9), 'rsi_low'] = df['rsi_low'] -8
-    # Computing action to be taken (1 = buy, -1 = sell, 0 = stay)
-    position = 0
-    df['position'] = 0
-    for index, row in df.iterrows():
-        if ((row["rsi"]) < (row["rsi_low"])) & (position == 0):
-            df.loc[index, 'position'] = 1
-            position = 1
-        elif ((row["rsi"]) > (row["rsi_high"])) & (position == 1):
-            df.loc[index, 'position'] = -1
-            position = 0
-    # Backtesting: Computing revenues
-    alloc = 100
-    df['net_position'] = df.position.cumsum().shift().fillna(0)
-    df['change'] = (df.close.pct_change() * df.net_position + 1).cumprod() * alloc
-    dfeth = df.tail(1)
-    #The same for NANO
-    binance_symbols = ["NANOUSDT"]
-    for symbol in binance_symbols:
-        get_all_binance(symbol, '5m', save = True)
-    df = pd.read_csv(r"NANOUSDT-5m-data.csv", parse_dates=True)
-    #df.drop_duplicates(subset ="timestamp", keep='first', inplace = True)
-    df = df.tail(8640)
-    #Computing indicators SMA and ADX, and whether trend is bear or bull according to sma
-    df['sma_fast'] = ta.SMA(df['close'],95)
-    df['sma_slow'] = ta.SMA(df['close'],200)
-    df['upper_band'], df['middle_band'], df['lower_band'] = ta.BBANDS(df['close'], timeperiod =20)
-    df['rsi'] = ta.RSI(df['close'],15)
-    df['adx'] = ta.ADX(df.high, df.low, df.close, timeperiod=3)
-    df['bear0bull1'] = np.where((df["sma_fast"] < df["sma_slow"]), 0, 1)
-    # Computing if trend is real according to number of consecutive rows with equal values in bear0bull1 column        
-    df['uptrend'] = 0
-    from itertools import islice
-    for index, row in islice(df.iterrows(), 1, None):
-        if ((row["bear0bull1"]) == 1):
-            df.loc[index, 'uptrend'] = df.loc[index - 1, 'uptrend'] +1
-        else:
-            df.loc[index, 'uptrend'] = 0
-    # Computing RSI so that it is different according to the value in uptrend column
-    for index, row in df.iterrows():
-        if ((row["uptrend"]) > 9):
-            df['rsi'] = ta.RSI(df['close'],10)
-    # Computing general tresholds for RSI according to uptrend column 
-    df['rsi_high'] = np.where((df["uptrend"] < 10), 50, 75)
-    df['rsi_low'] = np.where((df["uptrend"] < 10), 30, 60)
-    # Adjusting RSI treshold according to extreme values of ADX 
-    df.loc[(df['adx'] > 70) & (df['uptrend'] < 10), 'rsi_high'] = df['rsi_high'] + 20 #+20
-    df.loc[(df['adx'] < 50) & (df['uptrend'] < 10), 'rsi_low'] = df['rsi_low'] -8
-    df.loc[(df['adx'] > 70) & (df['uptrend'] > 9), 'rsi_high'] = df['rsi_high'] + 7
-    df.loc[(df['adx'] < 50) & (df['uptrend'] > 9), 'rsi_low'] = df['rsi_low'] -8
-    # Computing action to be taken (1 = buy, -1 = sell, 0 = stay)
-    position = 0
-    df['position'] = 0
-    for index, row in df.iterrows():
-        if ((row["rsi"]) < (row["rsi_low"])) & (position == 0):
-            df.loc[index, 'position'] = 1
-            position = 1
-        elif ((row["rsi"]) > (row["rsi_high"])) & (position == 1):
-            df.loc[index, 'position'] = -1
-            position = 0
-    # Backtesting: Computing revenues
-    alloc = 100
-    df['net_position'] = df.position.cumsum().shift().fillna(0)
-    df['change'] = (df.close.pct_change() * df.net_position + 1).cumprod() * alloc
-    dfnano = df.tail(1)
-    # Taking actions
-    t = time.localtime()
-    current_time = time.strftime("%H:%M:%S", t)
-    balanceusdt = client.get_asset_balance(asset='USDT')
-    balanceusdt = float(balanceusdt['free'])
-    balancenano = client.get_asset_balance(asset='NANO')
-    balancenano = float(balancenano['free'])
-    balanceeth = client.get_asset_balance(asset='ETH')
-    balanceeth = float(balanceeth['free'])   
-    if (dfnano.iloc[0,23] == 1) & (balanceusdt > 10):
-        print("I am going to buy NANO") 
-        print(current_time)
-        balance = client.get_asset_balance(asset='USDT')
-        trades = client.get_recent_trades(symbol='NANOUSDT')
-        quantity = (float(balance['free']))/float(trades[0]['price'])*0.9995
-        quantity = round(quantity, 2)
-        bought = quantity
-        client.order_market_buy(symbol='NANOUSDT', quantity=quantity) 
-    if (dfeth.iloc[0,23] == 1) & (balanceusdt > 10):
-        print("I am going to buy ETH") 
-        print(current_time)
-        balance = client.get_asset_balance(asset='USDT')
-        trades = client.get_recent_trades(symbol='ETHUSDT')
-        quantity = (float(balance['free']))/float(trades[0]['price'])*0.9995
-        quantity = round(quantity, 2)
-        bought = quantity
-        client.order_market_buy(symbol='ETHUSDT', quantity=quantity)    
-    if (dfeth.iloc[0, 23] == -1) & (balanceeth > 0.1):
-        print("I am going to sell ETH") 
-        print(current_time)
-        quantity=bought
-        bought=0
-        quantity = round(quantity, 2)
-        symbol = "ETHUSDT"
-        client.order_market_sell(symbol=symbol,quantity=quantity) 
-    if (dfeth.iloc[0, 23] == -1) & (balancenano > 10):
-        print("I am going to sell NANO") 
-        print(current_time)
-        quantity=bought
-        bought=0
-        quantity = round(quantity, 2)
-        symbol = "NANOUSDT"
-        client.order_market_sell(symbol=symbol,quantity=quantity) 
-    print(current_time)
-    s.enter(300, 1, f, (sc,))
 
-    
-# Creating the schedule
-s = sched.scheduler(time.time, time.sleep)
-s.enter(1, 1, f, (s,))
-s.run()
+binance_symbols = ["ETHUSDT"]
+for symbol in binance_symbols:
+    df = get_all_binance(symbol, '5m', save = True)
+#df = pd.read_csv(r"C:\Users\io\Desktop\DO_NOT_TOUCH\ETHUSDT-5m-data.csv", parse_dates=True)
+#df.drop_duplicates(subset ="timestamp", keep='first', inplace = True)
+df = df.tail(8640)
+#Computing indicators SMA and ADX, and whether trend is bear or bull according to sma
+df['sma_fast'] = ta.SMA(df['close'],95)
+df['sma_slow'] = ta.SMA(df['close'],200)
+df['upper_band'], df['middle_band'], df['lower_band'] = ta.BBANDS(df['close'], timeperiod =20)
+df['rsi'] = ta.RSI(df['close'],15)
+df['adx'] = ta.ADX(df.high, df.low, df.close, timeperiod=3)
+df['bear0bull1'] = np.where((df["sma_fast"] < df["sma_slow"]), 0, 1)
+# Computing if trend is real according to number of consecutive rows with equal values in bear0bull1 column        
+df['uptrend'] = 0
+from itertools import islice
+for index, row in islice(df.iterrows(), 1, None):
+    if ((row["bear0bull1"]) == 1):
+        df.loc[index, 'uptrend'] = df.loc[index - 1, 'uptrend'] +1
+    else:
+        df.loc[index, 'uptrend'] = 0
+# Computing RSI so that it is different according to the value in uptrend column
+for index, row in df.iterrows():
+    if ((row["uptrend"]) > 9):
+        df['rsi'] = ta.RSI(df['close'],10)
+# Computing general tresholds for RSI according to uptrend column 
+df['rsi_high'] = np.where((df["uptrend"] < 10), 50, 75)
+df['rsi_low'] = np.where((df["uptrend"] < 10), 30, 60)
+# Adjusting RSI treshold according to extreme values of ADX 
+df.loc[(df['adx'] > 70) & (df['uptrend'] < 10), 'rsi_high'] = df['rsi_high'] + 10 #+20
+df.loc[(df['adx'] < 50) & (df['uptrend'] < 10), 'rsi_low'] = df['rsi_low'] -8
+df.loc[(df['adx'] > 70) & (df['uptrend'] > 9), 'rsi_high'] = df['rsi_high'] + 7
+df.loc[(df['adx'] < 50) & (df['uptrend'] > 9), 'rsi_low'] = df['rsi_low'] -8
+# Computing action to be taken (1 = buy, -1 = sell, 0 = stay)
+position = 0
+df['position'] = 0
+for index, row in df.iterrows():
+    if ((row["rsi"]) < (row["rsi_low"])) & (position == 0):
+        df.loc[index, 'position'] = 1
+        position = 1
+    elif ((row["rsi"]) > (row["rsi_high"])) & (position == 1):
+        df.loc[index, 'position'] = -1
+        position = 0
+# Backtesting: Computing revenues
+alloc = 100
+df['net_position'] = df.position.cumsum().shift().fillna(0)
+df['change'] = (df.close.pct_change() * df.net_position + 1).cumprod() * alloc
+dfeth = df.tail(1)
+#The same for NANO
+binance_symbols = ["NANOUSDT"]
+for symbol in binance_symbols:
+    df = get_all_binance(symbol, '5m', save = True)
+#df = pd.read_csv(r"C:\Users\io\Desktop\DO_NOT_TOUCH\NANOUSDT-5m-data.csv", parse_dates=True)
+#df.drop_duplicates(subset ="timestamp", keep='first', inplace = True)
+df = df.tail(8640)
+#Computing indicators SMA and ADX, and whether trend is bear or bull according to sma
+df['sma_fast'] = ta.SMA(df['close'],95)
+df['sma_slow'] = ta.SMA(df['close'],200)
+df['upper_band'], df['middle_band'], df['lower_band'] = ta.BBANDS(df['close'], timeperiod =20)
+df['rsi'] = ta.RSI(df['close'],15)
+df['adx'] = ta.ADX(df.high, df.low, df.close, timeperiod=3)
+df['bear0bull1'] = np.where((df["sma_fast"] < df["sma_slow"]), 0, 1)
+# Computing if trend is real according to number of consecutive rows with equal values in bear0bull1 column        
+df['uptrend'] = 0
+for index, row in islice(df.iterrows(), 1, None):
+    if ((row["bear0bull1"]) == 1):
+        df.loc[index, 'uptrend'] = df.loc[index - 1, 'uptrend'] +1
+    else:
+        df.loc[index, 'uptrend'] = 0
+# Computing RSI so that it is different according to the value in uptrend column
+for index, row in df.iterrows():
+    if ((row["uptrend"]) > 9):
+        df['rsi'] = ta.RSI(df['close'],10)
+# Computing general tresholds for RSI according to uptrend column 
+df['rsi_high'] = np.where((df["uptrend"] < 10), 50, 75)
+df['rsi_low'] = np.where((df["uptrend"] < 10), 30, 60)
+# Adjusting RSI treshold according to extreme values of ADX 
+df.loc[(df['adx'] > 70) & (df['uptrend'] < 10), 'rsi_high'] = df['rsi_high'] + 20 #+20
+df.loc[(df['adx'] < 50) & (df['uptrend'] < 10), 'rsi_low'] = df['rsi_low'] -8
+df.loc[(df['adx'] > 70) & (df['uptrend'] > 9), 'rsi_high'] = df['rsi_high'] + 7
+df.loc[(df['adx'] < 50) & (df['uptrend'] > 9), 'rsi_low'] = df['rsi_low'] -8
+# Computing action to be taken (1 = buy, -1 = sell, 0 = stay)
+position = 0
+df['position'] = 0
+for index, row in df.iterrows():
+    if ((row["rsi"]) < (row["rsi_low"])) & (position == 0):
+        df.loc[index, 'position'] = 1
+        position = 1
+    elif ((row["rsi"]) > (row["rsi_high"])) & (position == 1):
+        df.loc[index, 'position'] = -1
+        position = 0
+# Backtesting: Computing revenues
+alloc = 100
+df['net_position'] = df.position.cumsum().shift().fillna(0)
+df['change'] = (df.close.pct_change() * df.net_position + 1).cumprod() * alloc
+dfnano = df.tail(1)
+# Taking actions
+t = time.localtime()
+current_time = time.strftime("%H:%M:%S", t)
+balanceusdt = client.get_asset_balance(asset='USDT')
+balanceusdt = float(balanceusdt['free'])
+balancenano = client.get_asset_balance(asset='NANO')
+balancenano = float(balancenano['free'])
+balanceeth = client.get_asset_balance(asset='ETH')
+balanceeth = float(balanceeth['free'])   
+if (dfnano.iloc[0,23] == 1) & (balanceusdt > 10):
+    print("I am going to buy NANO") 
+    print(current_time)
+    balance = client.get_asset_balance(asset='USDT')
+    trades = client.get_recent_trades(symbol='NANOUSDT')
+    quantity = (float(balance['free']))/float(trades[0]['price'])*0.9995
+    quantity = round(quantity, 2)
+    bought = quantity
+    client.order_market_buy(symbol='NANOUSDT', quantity=quantity) 
+if (dfeth.iloc[0,23] == 1) & (balanceusdt > 10):
+    print("I am going to buy ETH") 
+    print(current_time)
+    balance = client.get_asset_balance(asset='USDT')
+    trades = client.get_recent_trades(symbol='ETHUSDT')
+    quantity = (float(balance['free']))/float(trades[0]['price'])*0.9995
+    quantity = round(quantity, 2)
+    bought = quantity
+    client.order_market_buy(symbol='ETHUSDT', quantity=quantity)    
+if (dfeth.iloc[0, 23] == -1) & (balanceeth > 0.1):
+    print("I am going to sell ETH") 
+    print(current_time)
+    quantity=bought
+    bought=0
+    quantity = round(quantity, 2)
+    symbol = "ETHUSDT"
+    client.order_market_sell(symbol=symbol,quantity=quantity) 
+if (dfeth.iloc[0, 23] == -1) & (balancenano > 10):
+    print("I am going to sell NANO") 
+    print(current_time)
+    quantity=bought
+    bought=0
+    quantity = round(quantity, 2)
+    symbol = "NANOUSDT"
+    client.order_market_sell(symbol=symbol,quantity=quantity) 
+print(current_time)
