b47cf8851428b33bdca5b22286f317fc3d9bc238
smartdevello <77758615+smartdevello@users.noreply.github.com>
2021-01-20 20:37:44 -0500

    add keys.py

---
+++ keys.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 15 16:34:45 2021
+
+@author: io
+"""
+
+APIkey = "oiTVnwcAW1I55K93Ws1VKncjvsLjtQGYZmQ0BJyM4TGFa0i7QTucBzxW9zBnbpOk"
+APIsecret = "Dw9G6s9DKbgNxo084Cvrj9kUnXLsjRoptyjVwiaDJpGrZUjk9iUP7RyYaBUljOIe"
